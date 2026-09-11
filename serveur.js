98a58c9636accab2012dcecb91e068e5e09d1fac
lk140 <max.hemp@icloud.com>
2026-09-11 13:42:43 +0000

    début

---
+++ serveur.js
@@ -0,0 +1,14 @@
+console.log(2 + 3)
+
+const nom = "Albert" // string
+let age = 18  // int
+let aLePermis = false // bool 
+let temperature = 12.3 //float
+const note = [12, 13, 14] // tableau 
+let voiture = {
+    couleur: "rouge",
+    siege: "cuir",
+    volant: "moumoute"
+}                      // object
+
+console.log(voiture.volant) 
